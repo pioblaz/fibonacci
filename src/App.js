cbb2058ab299b521e231f10500c87b99245c41d7
pioblaz <piotrblazewicz10@wp.pl>
2021-12-04 13:02:13 +0100

    zaktualizowana aplikacja, zmieniony travis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(fibo(inputs.kword));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 123 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <p>
+            Podaj k:
+          </p>
+          <p>
+              <input
+              type="number"
+              min="0"
+              max="40"
+              name="kword"
+              value={inputs.kword || ""}
+              onChange={handleChange}
+              />
+          </p>
+          <input type="submit" value="Oblicz"/>
+        </form>
       </header>
     </div>
   );
+}
+
+function fibo(n) {
+  if (n==0) return 0;
+  if (n==1) return 1;
+  return fibo(n-1)+fibo(n-2);
 }
 
 export default App;
